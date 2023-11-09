d6ba8fe4c27c2dfcf778b4629f3e37b1930f528a
aillos <91605277+aillos@users.noreply.github.com>
2023-11-09 01:25:22 +0100

    Added method for no duplicates and no zero

---
+++ removeDupeZero.py
@@ -0,0 +1,17 @@
+import csv
+from more_itertools import unique_everseen
+
+input_file_path = './FinnCrawler/house_listings.csv'
+output_file_path = 'house_listing_nodup_nozero.csv'
+
+with open(input_file_path, 'r', newline='') as f, open(output_file_path, 'w', newline='') as out_file:
+    reader = csv.DictReader(f)
+    writer = csv.DictWriter(out_file, fieldnames=reader.fieldnames)
+
+    writer.writeheader()
+
+    unique_rows = unique_everseen(reader, key=lambda row: tuple(row.values()))
+
+    for row in unique_rows:
+        if row['totalPrice'] != '0' and row['usableArea'] != '0':
+            writer.writerow(row)
